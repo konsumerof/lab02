1b380ccab0cb5da9b971a285b258e82a43dfc8d8
konsumerof <matveystudy@gmail.com>
2022-03-21 11:45:34 +0300

    Clang-format should work

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 
-int main(){
-std::string name; // переменная типа string для хранения имени
-std::cout <<"Enter your name, sir: ";
-std::cin >> name; // запись имени в переменную
-std::cout <<"Hello world from " << name << std::endl; // вывод имени
-return 0;
+int main() {
+  std::string name; // переменная типа string для хранения имени
+  std::cout << "Enter your name, sir: ";
+  std::cin >> name; // запись имени в переменную
+  std::cout << "Hello world from " << name << std::endl; // вывод имени
+  return 0;
 }
