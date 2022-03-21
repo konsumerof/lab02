02fd9cf2d8c4660ca67bea5fb4cf3baa7ee00d17
konsumerof <matveystudy@gmail.com>
2022-03-21 11:02:55 +0300

    Fourth try of writing Hello world

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main(){
-std::string name;
+std::string name; // переменная типа string для хранения имени
 std::cout <<"Enter your name, sir: ";
-std::cin >> name;
-std::cout <<"Hello world from " << name << std::endl;
+std::cin >> name; // запись имени в переменную
+std::cout <<"Hello world from " << name << std::endl; // вывод имени
 return 0;
 }
